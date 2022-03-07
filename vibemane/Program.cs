65b36652810e357a5440902b4ba5e7ad6f4a507f
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-07 23:22:47 +0100

    Add project files.

---
+++ vibemane/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vibemane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/Boris Polak je stejně frajer");
+            Console.WriteLine("//Boris Polak je stejně frajer");
+            Console.WriteLine("///Boris Polak je stejně frajer");
+            Console.WriteLine("////Boris Polak je stejně frajer");
+            Console.WriteLine("/////Boris Polak je stejně frajer");
+            Console.WriteLine("//////Boris Polak je stejně frajer");
+            Console.WriteLine("///////Boris Polak je stejně frajer");
+            Console.WriteLine("////////Boris Polak je stejně frajer");
+            Console.WriteLine("/////////Boris Polak je stejně frajer");
+            Console.WriteLine("///////Boris Polak je stejně frajer");
+            Console.WriteLine("//////Boris Polak je stejně frajer");
+            Console.WriteLine("/////Boris Polak je stejně frajer");
+            Console.WriteLine("////Boris Polak je stejně frajer");
+            Console.WriteLine("///Boris Polak je stejně frajer");
+            Console.WriteLine("//Boris Polak je stejně frajer");
+            Console.WriteLine("/Boris Polak je stejně frajer");
+
+            Console.ReadLine();
+        }
+    }
+}
